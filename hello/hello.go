0f1d23525afcd038fbfd40143d491f7b0a7335ab
yuxi-o <88wangqh@163.com>
2019-05-15 20:38:04 -0700

    finish: How to write go code

--- hello/hello.go
+++ hello/hello.go
@@ -1,6 +1,10 @@
 package main
-import "fmt"
+import (
+	"fmt"
+	"github.com/yuxi-o/golang/stringutil"
+)
 
 func main() {
-	fmt.Println("Hello, Go world")
+//	fmt.Println("Hello, Go world")
+	fmt.Println(stringutil.Reverse("!oG, olleH"))
 }
