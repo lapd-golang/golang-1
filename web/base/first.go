45f191bab9ab02e2580aad63080540b0d67adb67
yuxi-o <88wangqh@163.com>
2020-03-24 14:45:04 +0800

    add one method for http.Handle

--- web/base/first.go
+++ web/base/first.go
@@ -14,12 +14,18 @@ func (f fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Hello, first!\n")
 }
 
+func zoo(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Hello, zoo!\n")
+}
+
 func main(){
 	http.Handle("/foo", fooHandler{})
 
 	http.HandleFunc("/bar", func(w http.ResponseWriter, r *http.Request){
 		fmt.Fprintf(w, "Hello, %q\n", r.URL.Path)
 	})
+
+	http.Handle("/zoo", http.HandlerFunc(zoo))
 
 	log.Fatal(http.ListenAndServe(":8088", nil))
 }
